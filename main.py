a0abd4019b933d822ee3e5f3c31e78d7ad2f1302
Georg <georgtsvetkof@gmail.com>
2025-04-12 17:20:20 +0300

    2

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(2)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", title="Главная страница")
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
