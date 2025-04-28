45460680fdd5cd535992b3c6856bf51de9b16ebd
Georg <georgtsvetkof@gmail.com>
2025-04-28 22:54:23 +0300

    add register form

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_login import LoginManager, login_user, login_required, logout_user
 from forms.loginform import LoginForm
+from forms.registerform import RegisterForm
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_very_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
@@ -13,16 +25,51 @@ def index():
                            styles_css=styles_css)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    styles_css = url_for('static', filename='css/register.css')
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form, styles_css=styles_css,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', styles_css=styles_css, form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     styles_css = url_for('static', filename='css/login.css')
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template("login.html", title='Авторизация', styles_css=styles_css, form=form)
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", styles_css=styles_css, form=form)
+    return render_template('login.html', title='Авторизация', styles_css=styles_css, form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
+    db_session.global_init("db/users.db")
     app.run()
 
 
